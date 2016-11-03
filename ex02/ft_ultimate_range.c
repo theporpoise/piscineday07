f702066f5cd77e8813a8e32fa5963cbeb83bd7ca
Matthew Gould <matt@everettgould.com>
2016-11-03 15:20:45 -0700

    finally figured out what i think they wanted for ultimate sort.  let's hope this is what they wanted

---
+++ ex02/ft_ultimate_range.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/02 20:38:35 by mgould            #+#    #+#             */
+/*   Updated: 2016/11/03 15:10:45 by mgould           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	*ft_range(int min, int max)
+{
+	int i;
+	int	*strints;
+
+	if (min >= max)
+		return (0);
+	i = 0;
+	strints = (int*)malloc(sizeof(int) * (max - min));
+	while ((i + min) < max)
+	{
+		strints[i] = min + i;
+		i++;
+	}
+	return (strints);
+}
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int size;
+
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	size = (max - min) * sizeof(int);
+	*range = ft_range(min, max);
+	return (size);
+}
